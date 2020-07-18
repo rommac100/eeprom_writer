0d08147c9eddca32832084524891036ca52b85c9
Christopher McCormick <rommac100@gmail.com>
2020-07-17 20:36:45 -0700

    Delete test.c

--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv)
-{
-	int size_bits = 5;	
-	int size_byte = (((double) size_bits)/8.00)+1;
-	printf("size scaled: %i\n", size_byte);
-	return 0;
-}
